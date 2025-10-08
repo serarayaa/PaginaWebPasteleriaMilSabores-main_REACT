865aec60df52d275b24288ce8ce2f1a4ef846e80
Sergio Araya Astudillo <ser.arayaa@duocuc.cl>
2025-10-07 21:03:37 -0300

    Corrección Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,31 @@
 export default function Login() {
-
   return (
     <div>
-       <main class="container py-5" style="max-width:560px;">
-      <h1 class="brand-font text-choco mb-4">Iniciar sesión</h1>
-      <form id="loginForm" class="needs-validation" novalidate>
-        <div class="mb-3">
-          <label class="form-label">Correo</label>
-          <input type="email" class="form-control" id="lemail" required/>
-          <div class="invalid-feedback">Ingresa tu correo.</div>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Contraseña</label>
-          <input type="password" class="form-control" id="lpass" required minlength="6" maxlength="32"/>
-          <div class="invalid-feedback">Ingresa tu contraseña.</div>
-        </div>
-        <button class="btn btn-choco" type="submit">Entrar</button>
-      </form>
-    </main>
+      <main className="container py-5" style={{ maxWidth: '560px' }}>
+        <h1 className="brand-font text-choco mb-4">Iniciar sesión</h1>
+        <form id="loginForm" className="needs-validation" noValidate>
+          <div className="mb-3">
+            <label className="form-label">Correo</label>
+            <input type="email" className="form-control" id="lemail" required />
+            <div className="invalid-feedback">Ingresa tu correo.</div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              id="lpass"
+              required
+              minLength="6"
+              maxLength="32"
+            />
+            <div className="invalid-feedback">Ingresa tu contraseña.</div>
+          </div>
+          <button className="btn btn-choco" type="submit">
+            Entrar
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
